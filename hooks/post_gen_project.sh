d5df9d81e2f29a377fbe7bba4d48ba6090e1ffb0
Eb-Zeero <macebelene@gmail.com>
2018-10-23 09:28:20 +0200

    fix hooks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,25 +11,4 @@ fi
 # install the required Node packages
 
 npm install --save-dev \
-    jest \
-    babel-core@^7.0.0-bridge.0 \
-    @babel/core \
-    @babel/preset-env \
-    babel-loader \
-    babel-jest \
-    regenerator-runtime \
-    eslint \
-    eslint-config-standard \
-    eslint-plugin-standard \
-    eslint-plugin-promise \
-    eslint-plugin-import \
-    eslint-plugin-node \
-    eslint-plugin-jest \
-    puppeteer \
-    jest-puppeteer \
-    express \
-    webpack \
-    webpack-cli \
-    webpack-dev-server \
-    webpack-merge \
-    mini-html-webpack-plugin
+    "@stencil/core": "~0.13.2"
